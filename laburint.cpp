34f7bb21591723fd2c65649e3a42a5f2f607774b
Reimo <reimotaavi.lobu@outlook.com>
2020-04-11 13:23:26 +0300

    esimesed sammud

--- laburint.cpp
+++ laburint.cpp
@@ -1,4 +1,27 @@
-//
-// Created by Reimo on 11.04.2020.
-//
+#include <iostream>
+#include <string>
+
+//protseduurid
+
+void loe_mootmed() {
+    //kusib kasutajalt pikkust ja laiust, mis on vahemikus 1-1000
+    unsigned int pikkus;
+    unsigned int laius;
+
+    std::cout << "Sisesta laburindi pikkus: ";
+    std::cin >> pikkus;
+    std::cout << "Sisesta laburindi laius: ";
+    std::cin >> laius;
+
+}
+
+void algvaartusta(std::string massiiv, unsigned int &pikkus, unsigned int &laius) {
+
+}
+
+//main kood
+
+int main() {
+    loe_mootmed();
+}
 
